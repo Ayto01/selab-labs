179d6f36d9c1cda99c59e1d887f2b6716ab3495b
Bekir Aytac Karakus <bekir.karakus@student.hogent.be>
2025-02-23 05:08:52 +0100

    Delete teamleden.sh

--- teamleden.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Name              | GitHub username"
-echo "--------------------------------------"
-echo "Bekir Aytac KARAKUS           | Ayto01"
-echo "Daniel CASTANO MONTEALEGRE      | username"
-echo "Sacha SCHREUER       | username"
-echo "Ilene DUBOIS        | username"
-echo "Francis OSTIJN      | username"
