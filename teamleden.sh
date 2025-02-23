2e358ca226c834c98289914c93022e7990227468
Bekir Karakus <bekir.karakus@student.hogent.be>
2025-02-23 05:07:42 +0100

    Teamleden

---
+++ teamleden.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Name              | GitHub username"
+echo "--------------------------------------"
+echo "Bekir Aytac KARAKUS           | Ayto01"
+echo "Daniel CASTANO MONTEALEGRE      | username"
+echo "Sacha SCHREUER       | username"
+echo "Ilene DUBOIS        | username"
+echo "Francis OSTIJN      | username"
